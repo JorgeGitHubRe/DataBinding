fc0c68cf55362b2835da3d29023daac82115fb08
Jorge <Jorge@DESKTOP-5DL7PLT>
2023-08-21 22:19:09 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding/DataBinding/Alumno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataBinding
+{
+    public class Alumno : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        string nombre = string.Empty;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (nombre == value)
+                    return;
+                nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+                OnPropertyChanged(nameof(MostrarNombre));
+            }
+        }
+
+        public string MostrarNombre => $"Nombre ingresado:{Nombre}";
+        void OnPropertyChanged(string nombre)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+}
